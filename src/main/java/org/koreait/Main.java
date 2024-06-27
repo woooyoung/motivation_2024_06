2d7e0fe8b238f7d18a1bfe8589c90d242594cc60
woooyoung <axdsw121@gmail.com>
2024-06-27 16:43:24 +0900

    종료 기능 구현, 등록 기능 구현중

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,15 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
     }
 }
+
