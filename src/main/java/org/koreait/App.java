ac427739bb02126bc45ede40ca7d13372a947015
woooyoung <axdsw121@gmail.com>
2024-06-28 11:44:57 +0900

    유지보수 및 협업을 위한 모듈 분리, 패키지 이동(리팩토링)

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,5 +1,7 @@
 package org.koreait;
 
+import org.koreait.motivation.entity.Motivation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
--- src/main/java/org/koreait/motivation/entity/Motivation.java
+++ src/main/java/org/koreait/motivation/entity/Motivation.java
@@ -1,4 +1,4 @@
-package org.koreait;
+package org.koreait.motivation.entity;
 
 public class Motivation {
     private int id;
