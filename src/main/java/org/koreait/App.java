65400efc7081cd2e3c09a28b721138fabd55611a
woooyoung <axdsw121@gmail.com>
2024-06-28 10:27:44 +0900

    motivation class 생성, add 시 List에 저장

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,5 +1,7 @@
 package org.koreait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,26 +12,18 @@ public class App {
         this.sc = sc;
     }
 
-    //    == motivation execution ==
-//    command) add
-//    motivation : 123
-//    source : qwe
-//1번 motivation이 등록되었습니다
-//    command) add
-//    motivation : 456
-//    source : asd
-//2번 motivation이 등록되었습니다
-//    command) list
-//== motivation list ==
-//    id   //   motivation   //  source
-//  2   //     456        //    asd
-//          1   //     123        //    qwe
-//    command) exit
-//== motivation end ==
     public void run() {
         System.out.println("== motivation execution ==");
 
-        int lastId = 1;
+        int lastId = 0;
+
+//        Motivation motivation0 = null;
+//        Motivation motivation1 = null;
+//        Motivation motivation2 = null;
+
+//        Motivation[] motivations = new Motivation[3];
+
+        List<Motivation> motivations = new ArrayList<>();
 
         while (true) {
             System.out.print("command) ");
@@ -44,19 +38,49 @@ public class App {
             }
 
             if (cmd.equals("add")) {
-                System.out.print("motivation : ");
-                String motivation = sc.nextLine();
+                int id = lastId + 1;
+                System.out.print("body : ");
+                String body = sc.nextLine();
                 System.out.print("source : ");
                 String source = sc.nextLine();
-                System.out.printf("%d번 motivation이 등록 되었습니다\n", lastId);
+
+                Motivation motivation = new Motivation(id, body, source);
+//                motivation.id = id;
+//                motivation.body = body;
+//                motivation.source = source;
+
+                motivations.add(motivation);
+
+//                motivations[id - 1] = motivation;
+
+//                if (motivation.id == 1) {
+//                    motivation0 = motivation;
+//                } else if (motivation.id == 2) {
+//                    motivation1 = motivation;
+//                } else if (motivation.id == 3) {
+//                    motivation2 = motivation;
+//                }
+
+                System.out.printf("%d번 motivation이 등록 되었습니다\n", id);
                 lastId++;
             } else if (cmd.equals("list")) {
                 System.out.println("== motivation list ==");
                 System.out.printf("  id   //   motivation   //  source   \n");
                 System.out.println("=".repeat(35));
+//                System.out.println(motivation2.toString());
+//                System.out.println(motivation1.toString());
+//                System.out.println(motivation0.toString());
+//                for (Motivation motivation : motivations) {
+//                    System.out.println(motivation.toString());
+//                }
+                if (motivations.size() == 0) {
+                    System.out.println("등록된 motivation 없음");
+                } else {
+                    System.out.println("있음");
+                    System.out.println("등록된 motivation 갯수 : " + motivations.size());
+                }
             }
         }
-
-
     }
 }
+
