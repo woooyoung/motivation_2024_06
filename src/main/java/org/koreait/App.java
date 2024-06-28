bf68df964f5c9fc6f7959191d27e39f476cf4f9c
woooyoung <axdsw121@gmail.com>
2024-06-28 09:53:06 +0900

    번호 증가 해결, list 명령어 생성

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -10,14 +10,33 @@ public class App {
         this.sc = sc;
     }
 
+    //    == motivation execution ==
+//    command) add
+//    motivation : 123
+//    source : qwe
+//1번 motivation이 등록되었습니다
+//    command) add
+//    motivation : 456
+//    source : asd
+//2번 motivation이 등록되었습니다
+//    command) list
+//== motivation list ==
+//    id   //   motivation   //  source
+//  2   //     456        //    asd
+//          1   //     123        //    qwe
+//    command) exit
+//== motivation end ==
     public void run() {
-        System.out.println("== motivation 실행 ==");
+        System.out.println("== motivation execution ==");
+
+        int lastId = 1;
+
         while (true) {
-            System.out.print("명령어) ");
+            System.out.print("command) ");
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("== motivation 종료 ==");
+                System.out.println("== motivation end ==");
                 break;
             } else if (cmd.length() == 0) {
                 System.out.println("명령어 입력해");
@@ -29,7 +48,12 @@ public class App {
                 String motivation = sc.nextLine();
                 System.out.print("source : ");
                 String source = sc.nextLine();
-                System.out.println("~번 motivation이 등록 되었습니다");
+                System.out.printf("%d번 motivation이 등록 되었습니다\n", lastId);
+                lastId++;
+            } else if (cmd.equals("list")) {
+                System.out.println("== motivation list ==");
+                System.out.printf("  id   //   motivation   //  source   \n");
+                System.out.println("=".repeat(35));
             }
         }
 
